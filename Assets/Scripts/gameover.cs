2af28a6163ce4e774e50084a64968fee126113e4
yuval-rahamim <76443702+yuval-rahamim@users.noreply.github.com>
2024-02-01 01:13:56 +0200

    Project Files

---
+++ Assets/Scripts/gameover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gameover : MonoBehaviour
+{
+    public Text pointt;
+    public static bool over;
+
+    public void Start()
+    {
+        gameObject.SetActive(false);
+        over = false;
+    }
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        over = true;
+        pointt.text = score.ToString() + " POINTS";
+        
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void MenueButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+}
---
+++ Assets/Scripts/scoreMan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreMan : MonoBehaviour
+{
+    public Text pointt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameover.over)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            pointt.text = "Score: " + carmoving.point.ToString();
+        }
+    }
+}
